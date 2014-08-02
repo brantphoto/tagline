1c3a68382238551ad47f97574cab591b0c3149f4
brantphoto <brant.barger@gmail.com>
2014-08-01 17:42:21 -0700

    Railingit add -Agit add -A

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,7 +1,41 @@
 class QuotesController < ApplicationController
-  def index
+def index
+  	@quotes = Quote.all
+  end
+
+  def show
+  	@quote = Quote.find(params[:id])
   end
 
   def new
+  	@quote = Quote.new
+  end
+
+  def create
+  	@quote = Quote.new(params.require(:bean).permit(:name, :roast, :origin, :quantity))
+  	if @quote.save
+  		redirect_to quotes_path
+  	else
+  		render 'new'
+  	end
+  end
+
+  def edit
+    @quote = Quote.find(params[:id]) 
+  end
+
+  def update
+    @quote = Quote.find(params[:id])
+    if @quote.update_attributes(params.require(:bean).permit(:name, :roast, :origin, :quantity))
+      redirect_to quotes_path 
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @quote = Quote.find(params[:id])
+    @quote.destroy
+    redirect_to quotes_path
   end
 end
