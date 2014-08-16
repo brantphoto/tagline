83492d4b6fdff6d71cd643ce853e35f515f957d8
brantphoto <brant.barger@gmail.com>
2014-08-15 18:57:05 -0700

    change of vote system :)

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,11 +4,14 @@ class VotesController < ApplicationController
 	 before_action :check_security
 
   def create
+    wipe_votes
   	vote = Vote.new
   	vote.user = current_user
   	vote.quote = @quote
   	if vote.save 
   		redirect_to location_quotes_path(@quote.location.id)
+    else
+      redirect_to location_quotes_path(@quote.location.id)
   	end
 
   end
@@ -19,12 +22,24 @@ class VotesController < ApplicationController
   def destroy
   end
 
+  def wipe_votes
+    @location = @quote.location
+    @location.quotes.each do |q|
+      q.votes.each do |v|
+        if v.user.id == current_user.id
+        v.destroy
+        end
+      end
+    end
+  end 
+
  private
  	def get_quote
  		@quote = Quote.find(params[:quote_id])
  	end
 
  	# def new_vote
+  
 
 
   def check_security
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,7 @@
 class Vote
   include Mongoid::Document
+  
+  validates :user_id, :uniqueness => {:scope => [:user_id, :quote_id]}
   belongs_to :quote
   belongs_to :user
 end
