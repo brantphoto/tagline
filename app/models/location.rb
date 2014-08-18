542734ff5452efc95062be60e54a33a908945f08
brantphoto <brant.barger@gmail.com>
2014-08-17 19:29:46 -0700

    styped new location form. Added fields to location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,12 +1,16 @@
 class Location
   include Mongoid::Document
   field :name, type: String
+  field :streetadd, type: String
+  field :city, type: String
+  field :state, type: String
   field :zipcode, type: String
 
   has_many :quotes
 
   def self.search(query)
-    where(:name => query) 
-    # where("name like ?" => "%#{query}%") 
+    # qlength = query.length
+    where(:name => query)
+    # where("name like ?" => query)
   end
 end
