0d04aa8ac75cf395cf673286b2c9267505d0729a
brantphoto <brant.barger@gmail.com>
2014-08-20 14:06:13 -0700

    added error handling to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  rescue_from Mongoid::Errors::DocumentNotFound do redirect_to root_url end
+  rescue_from ActionView::MissingTemplate do redirect_to root_url end
 
   helper_method :current_user
 
